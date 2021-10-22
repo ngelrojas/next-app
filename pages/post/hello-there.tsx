f461807436547d4b122a36b1af6bb7904048f9be
nge rojas <aomarrojasp@gmail.com>
2021-10-21 21:33:24 -0300

    finish scratch project

--- pages/post/hello-there.tsx
+++ pages/post/hello-there.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image'
+import Head from 'next/head'
+
 const Hello = () => (
-    <div>
+    <article>
+        <Head>
+            <title>hello :: this is my blog</title>
+            <meta property="og:title" content="hello there :: my blog " />
+        </Head>
         <h1>hello there...!</h1>
-    </div>
+        <Image 
+            alt="the image is iron man"
+            src="/post/hello-there/iron_man.jpg"
+            width={640}
+            height={427}
+        />
+    </article>
 )
 
 export default Hello
